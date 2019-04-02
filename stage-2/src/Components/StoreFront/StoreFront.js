5fc1796514e10a1b60cd4f4cf84b8a8d7bcdad58
Peter Hart <Oliverdog4@gmail.com>
2019-04-02 15:00:01 -0500

    Debuggo

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import './StoreFront.css';
 
 class StoreFront extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    let productDisplay = this.props.products.map((element, index) => {
+   let productDisplay ;
+    if (this.props.products) {
+    productDisplay = this.props.products.map((element, index) => {
       return (
         <div className="product-container" key={index}>
           <h2>{element.title}</h2>
@@ -13,9 +18,11 @@ class StoreFront extends Component {
           <button onClick={() => this.props.addToCart(element)}>Purchase!</button>
         </div>
       )
-    })
+    })}
+
     return (
       <div className="storefront-container">
+      <div>{productDisplay}</div>
       </div>
     )
   }
